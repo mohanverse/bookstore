8c3739a74ab549b43471ff35aad600f532d264ee
Mohan Kumar Chaudhary <98375650+mohanverse@users.noreply.github.com>
2023-10-01 13:06:32 +0530

    mohanverse

--- backend-book/config.js
+++ backend-book/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongoDBURI = 'mongodb+srv://mohanverse:mohanverse@cluster0.eg6vewu.mongodb.net/?retryWrites=true&w=majority';
+export const mongoDBURI = 'Your MongoDB URI';
